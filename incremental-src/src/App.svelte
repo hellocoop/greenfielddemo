df76e98d6d962d56e09c28f31a586d1782818bac
rohanharikr <rohanharikumar80@gmail.com>
2022-06-18 00:43:13 +0400

    design update

--- incremental-src/src/App.svelte
+++ incremental-src/src/App.svelte
@@ -146,12 +146,12 @@
 	<main class="z-50 relative pb-20">	
 	<section class="px-4 -mt-16 max-w-2xl mx-auto space-y-4">
 		{#if !isLoggedIn}
-			<li class="relative rounded-2xl bg-white h-32 border border-gray-700 flex items-center justify-center">
+			<li class="relative rounded-2xl bg-white shadow-lg p-8 flex items-center justify-center">
 				<button id="hello-login-btn" on:click={login} class:hello-btn-loader={loginAjax} class="hello-btn-white-on-light">ō&nbsp;&nbsp;&nbsp;Continue with Hellō</button>
 			</li>
 		{/if}
 
-		<li in:fly={{y: 40, delay: 0}} class:opacity-30={!isLoggedIn} class="rounded-2xl bg-white p-8 border border-gray-700">
+		<li in:fly={{y: 40, delay: 0}} class:opacity-30={!isLoggedIn} class="rounded-2xl bg-white shadow-lg p-8">
 			<div class="flex items-center justify-between">
 				<div class="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -172,7 +172,7 @@
 			{/if}
 		</li>
 
-		<li in:fly={{y: 40, delay: 100}} class:opacity-30={!isLoggedIn} class="opacity-30 rounded-2xl bg-white p-8 border border-gray-700">
+		<li in:fly={{y: 40, delay: 100}} class:opacity-30={!isLoggedIn} class="opacity-30 rounded-2xl bg-white shadow-lg p-8">
 			<div class="flex items-center">
 				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 					<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
@@ -201,7 +201,7 @@
 			{/if}
 		</li>
 
-		<li in:fly={{y: 40, delay: 200}} class:opacity-30={!isLoggedIn} class="opacity-30 rounded-2xl bg-white p-8 border border-gray-700">
+		<li in:fly={{y: 40, delay: 200}} class:opacity-30={!isLoggedIn} class="opacity-30 rounded-2xl bg-white shadow-lg p-8">
 			<div class="flex items-center">
 				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 					<path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
@@ -227,7 +227,7 @@
 			{/if}
 		</li>
 
-		<li in:fly={{y: 40, delay: 300}} class:opacity-30={!isLoggedIn} class="opacity-30 rounded-2xl bg-white p-8 border border-gray-700">
+		<li in:fly={{y: 40, delay: 300}} class:opacity-30={!isLoggedIn} class="opacity-30 rounded-2xl bg-white shadow-lg p-8">
 			<div class="flex items-center">
 				<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
 					<title>Ethereum</title>
